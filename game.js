e4a290bfae150ef88821f3aa2854863550f23442
João Marcelo Costa de Santana <159437965+jm-236@users.noreply.github.com>
2024-03-10 12:32:13 -0300

    sistema de vidas e tamanho aleatorio

--- game.js
+++ game.js
@@ -4,32 +4,69 @@ function ajustaTamanho() {
     l = window.innerWidth
 }
 
+function tamanhoAleatorio() {
+
+    tam = Math.floor(Math.random() * 3)
+    switch (tam){
+        case 0:
+            return 50
+
+        case 1:
+            return 75
+
+        case 2:
+            return 100
+    }
+}
+
 function iniciarJogo() {
     // início do jogo 
     document.getElementById("container").style.display = "none"
     var dificuldade = document.getElementById("dificuldade").value
-    console.log(dificuldade)
-    gerarMosquito()
+    window.location.href = "jogo.html";
+}
+
+function ladoAleatorio() {
+    return Math.floor(Math.random() * 2) == 0 ? "ScaleX(-1)" : "ScaleX(1)"
 }
 
 function gerarMosquito() {
+    if (document.getElementById("mosquito")){
+        document.getElementById("mosquito").remove()
+        
+        if (vidas > 0){
+        document.getElementById(`v${vidas}`).src = "imagens/coracao_vazio.png"
+        vidas--;
+        }
+    }
 
+
+    var tam = tamanhoAleatorio()
     // geração das coordenadas
     var coordY = Math.floor(Math.random() * h)
     var coordX = Math.floor(Math.random() * l)
+    coordX -= tam
+    coordY -= tam
+    coordY = coordY < 0 ? 0 : coordY
+    coordX = coordX < 0 ? 0 : coordX
 
     // criar o elemento HTML
     var mosquito = document.createElement("img")
     mosquito.src = "imagens/mosquito.png"
-    mosquito.width = "50"
-    mosquito.height = "50"
+    mosquito.id = "mosquito"
+    mosquito.width = tam
+    mosquito.height = tam
+    mosquito.style.transform = ladoAleatorio()
     mosquito.style.position = 'absolute'
     mosquito.style.marginLeft = `${coordX}px`
     mosquito.style.marginTop = `${coordY}px`
-    mosquito.onclick = gerarMosquito
+    mosquito.onclick = function(){
+        this.remove()
+    }
     document.body.appendChild(mosquito)
 }
 
 // Definir altura e largura
 h = window.innerHeight
 l = window.innerWidth
+var vidas = 3
